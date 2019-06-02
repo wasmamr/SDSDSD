dcee395acd219b3e6e2345191f38f95a22a333f8
wasmamr <49698892+wasmamr@users.noreply.github.com>
2019-06-02 07:03:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,28 +7,28 @@ client.on("ready", () => {
 let channel =     client.channels.get("498550129176084521")
 setInterval(function() {
 channel.send(`جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: `);
-}, 30)
+}, 10)
 })
 
 client.on("ready", () => {
 let channel =     client.channels.get("498537527041327120")
 setInterval(function() {
 channel.send(`جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: `);
-}, 30)
+}, 10)
 })
 
 client.on("ready", () => {
 let channel =     client.channels.get("498538064981786626")
 setInterval(function() {
 channel.send(`جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: `);
-}, 30)
+}, 10)
 })
 
 client.on("ready", () => {
 let channel =     client.channels.get("498537938116673536")
 setInterval(function() {
 channel.send(`جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: جري التهكير By WESO :frog: :dancer::skin-tone-3: `);
-}, 30)
+}, 10)
 })
 
 client.login(process.env.BOT_TOKEN);
